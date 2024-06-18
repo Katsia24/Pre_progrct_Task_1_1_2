1819081dad3a0787ca4bb55b7d0476725c92723e
Kate <katerinanizh@gmail.com>
2024-06-18 19:52:02 +0300

    Rename my project

--- src/main/java/org/example/Pre_project_Task_1_1_2.java
+++ src/main/java/org/example/Pre_project_Task_1_1_2.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Pre_progrct_Task_1_1_2 {
+public class Pre_project_Task_1_1_2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
